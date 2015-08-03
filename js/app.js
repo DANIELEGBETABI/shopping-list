95f372028d386998a1e5b9db0d09d3fcf8749a15
Haley Ressl <haleyressl@gmail.com>
2015-08-02 19:31:12 -0500

    modifying some images

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    //function getGrocery() {     - didn't need it ... it worked!
+    
         
         //append to list using the + button
         $("#addbutton").click(function(enter) {
@@ -17,22 +17,19 @@ $(document).ready(function() {
         });
     //};
     
-    //defined getGrocery, call it!
-    //getGrocery();
-    
-    //got the grocery, print it!
+    //got the grocery, print it
     function postGrocery() {
         var grocery = $("#groceries").val();
-        var listitem = '<li class="notbought"><img src="../images/check.jpg" alt="checkbox" id="checkbox">' +grocery+ '<input type="button" id="delete" value="check"> </li>'
+        var listitem = '<li class="notbought"><img src="../shopping-list/images/check.png" alt="checkbox" id="checkbox">' +grocery+ '<input type="button" id="delete" value="-"> </li>'
 
         if (grocery.indexOf(" ") !== -1 ) {
-            alert("Please enter something to get from the store.");
+            alert("Please enter a food item to add it to your list.");
         }
         else if (grocery == "") {
-            alert("Please enter something to get from the store.");
+            alert("Please enter a food item to add it to your list.");
         }
         else {
-            // $(".items").hide(grocery); - find a way to fade it in later ... 
+            
             $(".items").append(listitem).fadeIn(200);
             $("#groceries").val("");
             console.log(grocery);
@@ -45,26 +42,18 @@ $(document).ready(function() {
         $(this).closest("li").fadeOut(200);
     });
     
-    //toggle the checked or bought classes  .toggle didn't seem to work.
-    /* $(document).on("click", "#checkbox", function () {
-        if ($(this).closest("li").hasClass("notbought")) {
-            $(this).closest("li").addClass("checked").removeClass("notbought");
-        } else {
-            $(this).closest("li").removeClass("checked").addClass("notbought");
-        }
-    }); */
 
     //getting .toggle to work    
     $(document).on("click", "#checkbox", function () {
         $(this).closest("li").toggleClass("notbought checked");
     });
     
-    // time to get the clearall working!
+    // getting clearall to work
     $(document).on("click", "#clearall", function () {
         $(".items").empty();
     });
     
-    //adding a way to resize the instructions
+    //adding a way to resize the instructions fix this section later
     $('.instructions').hover(function(){
         $(this).attr('size',4);
     },function(){
